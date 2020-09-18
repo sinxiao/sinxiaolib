41bd7bad4ed277c1a01d2f61e4ac1461a625432c
xhj <a6t8@qq.com>
2020-09-18 18:19:50 +0800

    [fix]删除无用的工程

--- qrdlib/src/main/java/ae/sinxiao/android/qrd/core/ScanEvent.java
+++
@@ -1,9 +0,0 @@
-package ae.sinxiao.android.qrd.core;
-
-public class ScanEvent extends BaseEvent {
-    public static final int SCAN_RESULT_WEB_HANDLE = 4096;
-
-    public ScanEvent(int eventId) {
-        super(eventId);
-    }
-}
