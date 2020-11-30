14c614632eb101cf53d686f0c43d2a1e3edd8712
xhj <a6t8@qq.com>
2020-11-30 16:29:24 +0800

    [fix]显示Fragment增加多接口的显示

--- libcommon/src/main/java/com/xu/sinxiao/common/fragment/DefaultFragmentActivity.java
+++ libcommon/src/main/java/com/xu/sinxiao/common/fragment/DefaultFragmentActivity.java
@@ -14,6 +14,9 @@ import com.xu.sinxiao.common.R;
 import com.xu.sinxiao.common.databinding.DefaultFramelayoutBinding;
 import com.xu.sinxiao.common.mvp.BaseActivity;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultFragmentActivity extends BaseActivity {
@@ -32,13 +35,43 @@ public class DefaultFragmentActivity extends BaseActivity {
         context.startActivity(intent);
     }
 
+    public static void start(Context context, String name, HashMap<String, Object> values) {
+        Intent intent = new Intent(context, DefaultFragmentActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("page", name);
+        if (values != null && !values.isEmpty()) {
+            Iterator<String> iterator = values.keySet().iterator();
+            do {
+                String key = iterator.next();
+                Object value = values.get(key);
+                if (value instanceof String) {
+                    intent.putExtra(key, (String) value);
+                } else if (value instanceof Integer) {
+                    intent.putExtra(key, Integer.parseInt((String) value));
+                } else if (value instanceof Float) {
+                    intent.putExtra(key, (Float) value);
+                } else if (value instanceof Double) {
+                    intent.putExtra(key, (Double) value);
+                } else if (value instanceof Serializable) {
+                    intent.putExtra(key, (Serializable) value);
+                } else {
+//                    intent.putExtra(key, value);
+                }
+            } while (iterator.hasNext());
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String page = getIntent().getStringExtra("page");
+        Bundle bundle = getIntent().getExtras();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         Fragment fragment = concurrentHashMap.get(page);
+        if (bundle != null) {
+            fragment.setArguments(bundle);
+        }
         try {
             fragment = fragment.getClass().newInstance();
             defaultFramelayoutBinding = DataBindingUtil.setContentView(
@@ -50,6 +83,5 @@ public class DefaultFragmentActivity extends BaseActivity {
         } catch (InstantiationException e) {
             e.printStackTrace();
         }
-
     }
 }
