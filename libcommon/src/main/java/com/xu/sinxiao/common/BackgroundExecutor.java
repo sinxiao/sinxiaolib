18b6fa8e64aec479866f27e16b9ba9d14403c841
xhj <a6t8@qq.com>
2021-01-28 23:06:43 +0800

    [fix]修改后台线程初始化数

--- libcommon/src/main/java/com/xu/sinxiao/common/BackgroundExecutor.java
+++ libcommon/src/main/java/com/xu/sinxiao/common/BackgroundExecutor.java
@@ -16,7 +16,6 @@ public class BackgroundExecutor {
         public Thread newThread(Runnable r) {
             Thread thread = new Thread(r);
             thread.setPriority(Thread.MIN_PRIORITY);
-            Executors.defaultThreadFactory();
             return thread;
         }
     });
